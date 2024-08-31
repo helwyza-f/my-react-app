c4a42d30602750176124a77630a9716f628c8ae1
helwyza <helwyza@gmail.com>
2024-08-31 14:17:57 +0700

    belajar react di branch dev

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,20 +1,62 @@
+import { useState } from "react";
 const Home = () => {
+  const [mahasiswa, setMahasiswa] = useState({
+    nama: "Agus setiawan",
+    alamat: "Batam",
+    prodi: "Teknik Mesin",
+    fakultas: "Teknik Industri",
+  });
+  const [num, setNum] = useState(0);
+  const data = [
+    {
+      nama: "Bagas ",
+      alamat: "Jakarta",
+      prodi: "Teknik Sipil",
+      fakultas: "Teknik Industri",
+    },
+    {
+      nama: "Dodi pratowo",
+      alamat: "Jambi ",
+      prodi: "Teknik Komputer",
+      fakultas: "Teknologi Informatika",
+    },
+    {
+      nama: "Zainal abidin",
+      alamat: "Surabaya",
+      prodi: "Teknik Mesin",
+      fakultas: "Teknik Industri",
+    },
+  ];
+  function handlePrev() {
+    if (num > 0) setNum(num - 1);
+    setMahasiswa(data[num]);
+    console.log(num);
+  }
+  function handleNext() {
+    if (num != data.length - 1) setNum(num + 1);
+    setMahasiswa(data[num]);
+    console.log(num);
+  }
   return (
-    <div className="flex flex-col min-h-screen justify-center items-center">
-      <div className="flex size-40 items-center justify-center">
-        <div className="">
-          <h1>Home Page</h1>
-        </div>
-      </div>
-      <div className="">
-        <div className="">
-          <h2>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto,
-            natus!
-          </h2>
-        </div>
-      </div>
-    </div>
+    <>
+      <h1 className="text-xl mt-4 font-semibold pr-2">
+        Saya adalah {mahasiswa.nama}, tinggal di {mahasiswa.alamat}.<br></br>{" "}
+        Jurusan {mahasiswa.prodi} dari fakultas {mahasiswa.fakultas}.
+      </h1>
+      <hr />
+      <button
+        className="rounded-xl m-3 p-4 bg-slate-500 text-white font-bold"
+        onClick={() => handlePrev()}
+      >
+        Prev
+      </button>
+      <button
+        className="rounded-xl m-3 p-4 bg-blue-300 text-white font-bold"
+        onClick={() => handleNext()}
+      >
+        Next
+      </button>
+    </>
   );
 };
 
